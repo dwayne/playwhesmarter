86b3125067a2bf55f8a8be04bbedfff19fb1d3d2
Dwayne R. Crooks <me@dwaynecrooks.com>
2013-01-05 19:47:27 -0400

    Redesign the home page

---
+++ public/js/playwhesmarter.js
@@ -0,0 +1,71 @@
+function PlayWheResultsCtrl($scope, $http) {
+  $scope.results = [];
+
+  $scope.getResults = function () {
+    $http.jsonp('http://api.playwhesmarter.com/results?callback=JSON_CALLBACK&limit=3')
+      .success(function (data) {
+        $scope.results = data;
+      })
+      .error(function (data) {
+        /* TODO: Handle error condition */
+      });
+  }
+
+  $scope.getResults();
+}
+
+angular.module('PlayWheSmarter', [])
+  .filter('timeOfDay', function () {
+    return function (period, as_time) {
+      if (as_time) {
+        return { 1: '10:30 am', 2: '1:00 pm', 3: '6:30 pm' }[period];
+      } else {
+        return { 1: 'Morning', 2: 'Midday', 3: 'Evening' }[period];
+      }
+    };
+  })
+  .filter('spirit', function () {
+    var SPIRITS = [
+      {}, /* force 1-based access */
+      'centipede',
+      'old lady',
+      'carriage',
+      'dead man',
+      'parson man',
+      'belly',
+      'hog',
+      'tiger',
+      'cattle',
+      'monkey',
+      'corbeau',
+      'king',
+      'crapaud',
+      'money',
+      'sick woman',
+      'jamette',
+      'pigeon',
+      'water boat',
+      'horse',
+      'dog',
+      'mouth',
+      'rat',
+      'house',
+      'queen',
+      'morocoy',
+      'fowl',
+      'little snake',
+      'red fish',
+      'opium man',
+      'house cat',
+      'parson wife',
+      'shrimps',
+      'spider',
+      'blind man',
+      'big snake',
+      'donkey'
+    ];
+
+    return function (number) {
+      return SPIRITS[number];
+    };
+  });
